1fe58063a077a25c756205e90df0b6538028a5ab
Ritesh Singh <riteshsingh@Riteshs-MacBook-Pro.local>
2024-06-10 22:33:00 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
-import AnimeCursor from "./components/AnimeCursor";
 import ScrollToTop from "./components/ScrollToTop";
 import Carrier from "./pages/Carrier";
 import Shipper from "./pages/Shipper";
@@ -11,7 +10,6 @@ import BusinessOverview from "./pages/BusinessOverview";
 function App() {
   return (
     <div className="bg-black from-slate-900 via-purple-900 to-slate-900">
-      <AnimeCursor />
       <Header />
       <ScrollToTop />
       <Routes>
