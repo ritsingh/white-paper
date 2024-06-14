d3f0532fe58bdcd24c6df6ad8cf093f7ddc064bd
Ritesh Singh <riteshsingh@Riteshs-MacBook-Pro.local>
2024-06-14 17:55:06 +0530

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -69,7 +69,7 @@ function Header() {
           </li>
           <li className={ `border-r border-gray-500 lg:pr-5 relative group w-max ${selectedMenu === 2 ? 'hazmat-label' : ''}`}>
             <Link
-              // to={"/"}
+              to={"/esang"}
               onClick={() => setSelectedMenu(2)}
               className="flex gap-1 items-center justify-center cursor-pointer text-base lg:text-lg font-semibold before:absolute before:rounded-md before:left-0 before:-bottom-1 before:w-0 transition-all before:duration-200 before:h-[4px] before:bg-purple-500 group-hover:before:w-full"
             >
